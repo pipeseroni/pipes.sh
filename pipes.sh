580423913a9bf1fdd08aadc81f1bebe5761eabbc
Yu-Jie Lin <livibetter@gmail.com>
2013-02-01 11:20:30 +0800

    simplify some arithmetic evaluations, fix reset after x characters

--- pipes.sh
+++ pipes.sh
@@ -11,10 +11,10 @@ v=( [00]="\x83" [01]="\x8f" [03]="\x93"
 OPTIND=1
 while getopts "p:f:s:r:h" arg; do
 case $arg in
-    p) ((p=($OPTARG>0)?$OPTARG:$p));;
-    f) ((f=($OPTARG>19 && $OPTARG<101)?$OPTARG:$f));;
-    s) ((s=($OPTARG>4 && $OPTARG<16 )?$OPTARG:$s));;
-    r) ((r=($OPTARG>0)?$OPTARG:$r));;
+    p) ((p=(OPTARG>0)?OPTARG:p));;
+    f) ((f=(OPTARG>19 && OPTARG<101)?OPTARG:f));;
+    s) ((s=(OPTARG>4 && OPTARG<16 )?OPTARG:s));;
+    r) ((r=(OPTARG>0)?OPTARG:r));;
     h) echo -e "Usage: pipes [OPTION]..."
         echo -e "Animated pipes terminal screensaver.\n"
         echo -e " -p \tnumber of pipes (D=1)."
@@ -34,19 +34,19 @@ done
 tput smcup
 tput reset
 tput civis
-while ! read -t0.0$((1000/$f)) -n1; do
+while ! read -t0.0$((1000/f)) -n1; do
     for (( i=1; i<=p; i++ )); do
         # New position:
         ((${l[i]}%2)) && ((x[i]+=(${l[i]}==1)?1:-1))
         ((!(${l[i]}%2))) && ((y[i]+=(${l[i]}==2)?1:-1))
 
         # Loop on edges (change color on loop):
-        ((c[i]=(${x[i]}>$w || ${x[i]}<0 || ${y[i]}>$h || ${y[i]}<0)?($RANDOM%7-1):${c[i]}))
-        ((x[i]=(${x[i]}>$w)?0:((${x[i]}<0)?$w:${x[i]})))
-        ((y[i]=(${y[i]}>$h)?0:((${y[i]}<0)?$h:${y[i]})))
+        ((c[i]=(${x[i]}>w || ${x[i]}<0 || ${y[i]}>h || ${y[i]}<0)?(RANDOM%7-1):${c[i]}))
+        ((x[i]=(${x[i]}>w)?0:((${x[i]}<0)?w:${x[i]})))
+        ((y[i]=(${y[i]}>h)?0:((${y[i]}<0)?h:${y[i]})))
 
         # New random direction:
-        ((n[i]=$RANDOM%$s-1))
+        ((n[i]=RANDOM%s-1))
         ((n[i]=(${n[i]}>1||${n[i]}==0)?${l[i]}:${l[i]}+${n[i]}))
         ((n[i]=(${n[i]}<0)?3:${n[i]}%4))
 
@@ -55,6 +55,6 @@ while ! read -t0.0$((1000/$f)) -n1; do
         echo -ne "\033[1;3${c[i]}m\xe2\x94${v[${l[i]}${n[i]}]}"
         l[i]=${n[i]}
     done
-    (($t>$r)) && tput reset && tput civis && t=0 || ((t++))
+    ((t*p>=r)) && tput reset && tput civis && t=0 || ((t++))
 done
 tput rmcup
