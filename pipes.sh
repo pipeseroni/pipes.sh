e50c59395878fc1e3ce5f32005826dadbaffbcb3
Yu-Jie Lin <livibetter@gmail.com>
2013-02-01 12:13:32 +0800

    add -R random starting point option

--- pipes.sh
+++ pipes.sh
@@ -7,20 +7,23 @@ v=( [00]="\x83" [01]="\x8f" [03]="\x93"
     [10]="\x9b" [11]="\x81" [12]="\x93"
     [21]="\x97" [22]="\x83" [23]="\x9b"
     [30]="\x97" [32]="\x8f" [33]="\x81" )
+RNDSTART=0
 
 OPTIND=1
-while getopts "p:f:s:r:h" arg; do
+while getopts "p:f:s:r:Rh" arg; do
 case $arg in
     p) ((p=(OPTARG>0)?OPTARG:p));;
     f) ((f=(OPTARG>19 && OPTARG<101)?OPTARG:f));;
     s) ((s=(OPTARG>4 && OPTARG<16 )?OPTARG:s));;
     r) ((r=(OPTARG>0)?OPTARG:r));;
+    R) RNDSTART=1;;
     h) echo -e "Usage: pipes [OPTION]..."
         echo -e "Animated pipes terminal screensaver.\n"
-        echo -e " -p \tnumber of pipes (D=1)."
+        echo -e " -p [1-]\tnumber of pipes (D=1)."
         echo -e " -f [20-100]\tframerate (D=75)."
         echo -e " -s [5-15]\tprobability of a straight fitting (D=13)."
         echo -e " -r LIMIT\treset after x characters (D=2000)."
+        echo -e " -R \t\trandom starting point."
         echo -e " -h\t\thelp (this screen).\n"
         exit 0;;
     esac
@@ -28,7 +31,8 @@ done
 
 for (( i=1; i<=p; i++ )); do
     c[i]=$((i%7)) n[i]=0 l[i]=0
-    x[i]=$((w/2)) y[i]=$((h/2))
+    ((x[i]=RNDSTART==1?RANDOM*w/32768:w/2))
+    ((y[i]=RNDSTART==1?RANDOM*h/32768:h/2))
 done
 
 tput smcup
